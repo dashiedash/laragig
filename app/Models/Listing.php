dc35667ff120c7bd5cecd53addf1fe380d2156e0
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-17 16:34:48 +0800

    Creating a Factory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Dominus regit me, et nihil mihi deerit: in loco pascuae, ibi me collocavit. Super aquam refectionis educavit me; animam meam convertit. Deduxit me super semitas justitiae propter nomen suum. Nam etsi ambulavero in medio umbrae mortis, non timebo mala, quoniam tu mecum es. Virga tua, et baculus tuus, ipsa me consolata sunt. Parasti in conspectu meo mensam adversus eos qui tribulant me; impinguasti in oleo caput meum: et calix meus inebrians, quam praeclarus est! Et misericordia tua subsequetur me omnibus diebus vitae meae; et ut inhabitem in domo Domini in longitudinem dierum. '
-
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Dominus regit me, et nihil mihi deerit: in loco pascuae, ibi me collocavit. Super aquam refectionis educavit me; animam meam convertit. Deduxit me super semitas justitiae propter nomen suum. Nam etsi ambulavero in medio umbrae mortis, non timebo mala, quoniam tu mecum es. Virga tua, et baculus tuus, ipsa me consolata sunt. Parasti in conspectu meo mensam adversus eos qui tribulant me; impinguasti in oleo caput meum: et calix meus inebrians, quam praeclarus est! Et misericordia tua subsequetur me omnibus diebus vitae meae; et ut inhabitem in domo Domini in longitudinem dierum. '
-            ]
-        ];
-    }
-
-
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
