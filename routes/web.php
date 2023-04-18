32fb98bf941543c5b5525ebe71478c24437cdca8
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-18 10:51:36 +0800

    Finish Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,17 @@ use App\Models\Listing;
 
 // All Listings
 Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
+  return view('listings', [
+    'heading' => 'Latest Listings',
+    'listings' => Listing::all()
+  ]);
 });
 
 
 
 // Single Listing
-Route::get('/listings/{id}', function ($id) {
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
+Route::get('/listings/{listing}', function (Listing $listing) {
+  return view('listing', [
+    'listing' => $listing
+  ]);
 });
