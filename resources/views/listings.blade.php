dc3935fa6d4f3d340c3a4f7a6a3191bc8bb530c9
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-18 10:19:38 +0800

    Finish Adding the Theme HTML

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,27 +1,54 @@
 @extends('layout')
 
-
-
 @section('content')
+	@include('partials._hero')
+	<div class="mx-4 gap-4 space-y-4 md:space-y-0 lg:grid lg:grid-cols-2">
 
-<h1>{{$heading}}</h1>
+		@unless (count($listings) == 0)
+			@foreach ($listings as $listing)
+				<div class="rounded border border-gray-200 bg-gray-50 p-6">
 
-@unless (count($listings) == 0)
+					<div class="flex">
+						<img class="mr-6 hidden w-48 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
 
-@foreach ($listings as $listing)
-<h2>
-    <a href="/listings/{{$listing['id']}}">
-        {{ $listing['title'] }}
-    </a>
-</h2>
-<p>
-    {{ $listing['description'] }}
-</p>
-@endforeach
+						<div>
+							<h3 class="text-2xl">
+								<a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
+							</h3>
 
-@else
-<p>No listings found</p>
+							<div class="mb-4 text-xl font-bold">{{ $listing->company }}</div>
 
-@endunless
+							<ul class="flex">
+
+								<li class="mr-2 flex items-center justify-center rounded-xl bg-black py-1 px-3 text-xs text-white">
+									<a href="#">Laravel</a>
+								</li>
+
+								<li class="mr-2 flex items-center justify-center rounded-xl bg-black py-1 px-3 text-xs text-white">
+									<a href="#">API</a>
+								</li>
+
+								<li class="mr-2 flex items-center justify-center rounded-xl bg-black py-1 px-3 text-xs text-white">
+									<a href="#">Backend</a>
+								</li>
+
+								<li class="mr-2 flex items-center justify-center rounded-xl bg-black py-1 px-3 text-xs text-white">
+									<a href="#">Vue</a>
+								</li>
+
+							</ul>
+
+							<div class="mt-4 text-lg">
+								<i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+							</div>
+						</div>
+					</div>
+				</div>
+			@endforeach
+		@else
+			<p>No listings found</p>
+		@endunless
+
+	</div>
 
 @endsection
