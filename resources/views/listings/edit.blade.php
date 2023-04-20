da52a2107426f050b90b5b0b9945ca727ec6be86
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-20 16:01:45 +0800

    Finish Edit Listing

---
+++ resources/views/listings/edit.blade.php
@@ -0,0 +1,169 @@
+@extends('layout')
+
+@section('content')
+  <x-card class="mx-auto mt-24 max-w-lg rounded border border-gray-200 bg-gray-50 p-10">
+    <header class="text-center">
+      <h2 class="mb-1 text-2xl font-bold uppercase">
+        Edit a Gig
+      </h2>
+      <p class="mb-4">Edit {{ $listing->title }} at {{ $listing->company }}</p>
+    </header>
+
+    <form method="POST" action="/listings/{{ $listing->id }}" enctype="multipart/form-data">
+      @csrf
+      @method('PUT')
+      <div class="mb-6">
+        <label
+          for="company"
+          class="mb-2 inline-block text-lg">Company Name</label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="company"
+          value=" {{ $listing->company }} " />
+
+        @error('company')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label for="title" class="mb-2 inline-block text-lg">Job Title</label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="title"
+          value=" {{ $listing->title }} "
+          placeholder="Example: Senior Laravel Developer" />
+
+        @error('title')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label
+          for="location"
+          class="mb-2 inline-block text-lg">Job Location</label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="location"
+          value=" {{ $listing->location }} "
+          placeholder="Example: Remote, Boston MA, etc" />
+
+        @error('location')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label for="email" class="mb-2 inline-block text-lg">Contact Email</label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="email"
+          value=" {{ $listing->email }} " />
+
+        @error('email')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label
+          for="website"
+          class="mb-2 inline-block text-lg">
+          Website/Application URL
+        </label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="website"
+          value=" {{ $listing->website }} " />
+
+        @error('website')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label for="tags" class="mb-2 inline-block text-lg">
+          Tags (Comma Separated)
+        </label>
+        <input
+          type="text"
+          class="w-full rounded border border-gray-200 p-2"
+          name="tags"
+          value=" {{ $listing->tags }} "
+          placeholder="Example: Laravel, Backend, Postgres, etc" />
+
+        @error('tags')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label for="logo" class="mb-2 inline-block text-lg">
+          Company Logo
+        </label>
+        <input
+          type="file"
+          class="w-full rounded border border-gray-200 p-2"
+          name="logo" />
+
+        <img
+          class="mr-6 mb-6 w-48"
+          src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png') }}"
+          alt="" />
+
+        @error('logo')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label
+          for="description"
+          class="mb-2 inline-block text-lg">
+          Job Description
+        </label>
+        <textarea
+          class="w-full rounded border border-gray-200 p-2"
+          name="description"
+          rows="10"
+          placeholder="Include tasks, requirements, salary, etc">{{ $listing->description }}
+        </textarea>
+
+        @error('description')
+          <p class="mt-1 text-xs text-red-500">
+            {{ $message }}
+          </p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <button
+          class="bg-laravel rounded py-2 px-4 text-white hover:bg-black">
+          Update Gig
+        </button>
+
+        <a href="/" class="ml-4 text-black"> Back </a>
+      </div>
+    </form>
+  </x-card>
+@endsection
--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -48,5 +48,11 @@
         </div>
       </div>
     </x-card>
+
+    <x-card class="mt-4 flex space-x-6 p-2">
+      <a href="/listings/{{ $listing->id }}/edit">
+        <i class="fa-solid fa-pencil">Edit</i>
+      </a>
+    </x-card>
   </div>
 @endsection
