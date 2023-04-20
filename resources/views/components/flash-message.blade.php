4027e0c6cab52825e7450067ff56d3f501d98fbb
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-20 10:12:38 +0800

    Alpine.js For Message Removal

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,9 @@
 @if (session()->has('message'))
-  <div class="bg-laravel fixed top-0 left-1/2 -translate-x-1/2 transform px-48 py-3 text-white">
+  <div
+    x-data='{ show: true}'
+    x-init='setTimeout(() => show = false, 7000)'
+    x-show='show'
+    class="bg-laravel fixed top-0 left-1/2 -translate-x-1/2 transform px-48 py-3 text-white">
     <p>
       {{ session('message') }}
     </p>
