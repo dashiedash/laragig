6220c29de9996b5d04a402fab1724d0166e52e95
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2023-04-24 13:50:29 +0800

    Finish Add Ownership to Listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,26 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // \App\Models\User::factory(10)->create();
 
-        Listing::factory(6)->create();
-    }
+    $user = User::factory()->create([
+      'name' => 'John Doe',
+      'email' => 'john@gmail.com'
+    ]);
+
+    Listing::factory(6)->create([
+      'user_id' => $user->id
+    ]);
+  }
 }
